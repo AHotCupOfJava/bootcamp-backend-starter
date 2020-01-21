1c3d84bcf53eea4a6cf622d121392b4b24f98aba
Clarissa BS <cbriascostewart@college.harvard.edu>
2020-01-21 11:32:22 -0500

    bug fixes

--- data/preference.js
+++ data/preference.js
@@ -1,25 +1,19 @@
 const casual = require('casual')
-const userData = require ('./user')
-
+const userData = require('./user')
 
 
 casual.define('preference', ({ userId }) => ({
-  id: casual.uuid,
-
+  userId,
   searchBar: casual.boolean,
-  email: casual.email,
   weatherCur: casual.boolean,
   greeting: casual.boolean,
-  created_at: casual.moment,
-  updated_at: casual.moment,
-  userId,
 }))
 
 
 const preferenceData = []
 
 for (let i = 0; i < 20; ++i) {
-    const userId = casual.random_element(userData).id
+  const userId = casual.random_element(userData).id
   preferenceData.push(casual.preference({ userId }))
 }
 
